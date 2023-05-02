5c1b2aeba99bf9928726412ea2f64f036b9bc798
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-05-01 22:54:45 -0700

    worked on app and index

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Routes, Route, HashRouter as Router} from 'react-router-dom';
+import Main from './components/Main';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import './style.css'
 
 const App = () => {
 
-  const location = useLocation();
-
     return (
-        <div className=''>
-          <Header />
-          <Outlet location={location} key={location.pathname} />
-          <Footer />
+        <div>
+          <Router basename="/">
+            <Routes>
+              <Route>
+                <Route index='true' element={<Main />} />
+                <Route path="Main" element={<Main />} />
+                <Route path="AboutMe" element={<AboutMe />} />
+                <Route path="Portfolio" element={<Portfolio />} />
+                <Route path="Contact" element={<Contact />} />
+                <Route path="Resume" element={<Resume />} />
+              </Route>
+            </Routes>
+          </Router>,
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, HashRouter as Router, useLocation} from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
 import App from './App';
-import Main from './components/Main';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import './style.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
-      <Routes>
-        <Route exact path="/" element={<App />}>
-          <Route index='true' element={<Main />} />
-          <Route path="Main" element={<Main />} />
-          <Route path="AboutMe" element={<AboutMe />} />
-          <Route path="Portfolio" element={<Portfolio />} />
-          <Route path="Contact" element={<Contact />} />
-          <Route path="Resume" element={<Resume />} />
-        </Route>
-      </Routes>
-    </Router>,
+      <Header />
+        <App />
+      <Footer />
   </React.StrictMode>,
 document.getElementById('root'));
