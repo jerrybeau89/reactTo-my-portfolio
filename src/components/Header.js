ba0a0ac11a5a6ff4d854ab1b28b5fd79e60fc00b
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-06-04 08:09:06 -0700

    Fixed bug in offcanvas collapse

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,15 +6,11 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
-  const [show, setShow] = useState(false);
-
-  const toggleOffCanvas = () => {
-    setShow((show) => !show);
-  };
+  
 
   return (
     <>
-      <Navbar bg="dark" expand="lg" className="mb-3 sticky-top" >
+      <Navbar collapseOnSelect bg="dark" expand="lg" className="mb-3 sticky-top" >
         <Container fluid>
           <LinkContainer to="/">
             <Navbar.Brand className="text-light">
@@ -24,15 +20,12 @@ const Header = () => {
           <Navbar.Toggle
             className="bg-secondary"
             aria-controls={`offcanvasNavbar-expand-lg`}
-            onClick={toggleOffCanvas}
           />
           <Navbar.Offcanvas
             className="bg-dark text-light"
             id={`offcanvasNavbar-expand-lg`}
             aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
             placement="end"
-            show={show}
-            onHide={toggleOffCanvas}
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
@@ -42,19 +35,19 @@ const Header = () => {
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <LinkContainer to="/">
-                  <Nav.Link className="text-light" onClick={toggleOffCanvas}>Home</Nav.Link>
+                  <Nav.Link className="text-light">Home</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="/aboutme">
-                  <Nav.Link className="text-light" onClick={toggleOffCanvas}>About Me</Nav.Link>
+                  <Nav.Link className="text-light">About Me</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="/portfolio">
-                  <Nav.Link className="text-light" onClick={toggleOffCanvas}>Portfolio</Nav.Link>
+                  <Nav.Link className="text-light">Portfolio</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="/contact">
-                  <Nav.Link className="text-light" onClick={toggleOffCanvas}>Contact</Nav.Link>
+                  <Nav.Link className="text-light">Contact</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="/resume">
-                  <Nav.Link className="text-light" onClick={toggleOffCanvas}>Resume</Nav.Link>
+                  <Nav.Link className="text-light">Resume</Nav.Link>
                 </LinkContainer>
               </Nav>
             </Offcanvas.Body>
